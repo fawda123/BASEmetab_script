100231ba9a25f5ff549f1dec08affbed96303200
fawda123 <mbafs2012@gmail.com>
2020-10-16 17:30:56 -0400

    apnerr prepped I think for basemetab

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -3,6 +3,19 @@ library(lubridate)
 
 # format 2012 AP data for use with BASEmetab ------------------------------
 
-APNERR2012 <- read.csv('data/APNERR2012.csv')
+# totpar is mmol/m2 total for 15 minute obs, umol/m2/s
+# sal is ppt, should be ppt
+# DO is mg/l, should be mg/l
+# temp is C, should be C
+# BP is mb, should be atm
+APNERR2012 <- read.csv('data/APNERR2012.csv') %>% 
+  mutate(
+    DateTimeStamp = as.character(DateTimeStamp), 
+    totpar = ifelse(is.na(totpar), 0, totpar), 
+    totpar = totpar * 1000 / (15 * 60), # convert to umol and per second
+    BP = BP / 1013 # mb to atm
+  ) %>% 
+  separate(DateTimeStamp, c('Date', 'Time'), sep = ' ') %>% 
+  select(Date, Time, I = totpar, tempc = ATemp, DO.meas = DO_obs, atmo.pressure = BP, salinity = Sal)
 
-# make APNERR look like yallakool
+save(APNERR2012, file = 'data/APNERR2012.RData', compress = 'xz')

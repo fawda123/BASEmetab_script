6cf94fc1207f9e90507f0a26ba6628630914a377
fawda123 <mbafs2012@gmail.com>
2020-11-18 10:13:24 -0500

    reprocess BASE with correct depth for AP cat

--- script2.R
+++ script2.R
@@ -36,15 +36,3 @@ kvol <- Kareal / H
 
 results <- bayesmetab(data.dir, results.dir, interval = 900, K.est = F, K.meas.mean = kvol, K.meas.sd = 1e-6)
 
-# results <- read.csv('~/Desktop/BASE_results_2020-11-15 135525.csv')
-toplo <- results %>% 
-  # filter(R.Rhat < 1.1) %>%
-  # filter(PPP > 0.4) %>%
-  mutate(ER.mean = -1 * ER.mean) %>% 
-  select(Date, GPP.mean, ER.mean, NEP.mean) %>% 
-  mutate(Date = lubridate::ymd(Date)) %>% 
-  gather('var', 'val', -Date)
-
-ggplot(toplo, aes(x = Date, y = val, colour = var)) + 
-  geom_line() + 
-  geom_point()

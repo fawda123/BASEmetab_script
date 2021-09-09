b7093bf2c966faafdb5328feefba09838e2dbf44
fawda123 <mbafs2012@gmail.com>
2021-09-09 17:29:48 -0400

    working eval cor app, need to deploy

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -48,3 +48,53 @@ APNERR2012dtd <- DO_APNERR2012_6_12_0.8 %>%
 write.csv(APNERR2012dtd, file = 'data/APNERR2012dtd.csv', row.names = F)
 save(APNERR2012dtd, file = 'data/APNERR2012dtd.RData', compress = 'xz')
 
+# apa observed with cordat floored sun angle ------------------------------
+
+# original apaobs created in appalachicola.Rmd
+
+box::use(
+  WtRegDO[ecometab, wtreg, meteval, met_day_fun, evalcor],
+  here[...], 
+  doParallel[registerDoParallel], 
+  parallel[detectCores],
+  dplyr[...],
+  tidyr[...],
+  SWMPr[import_local, qaqc, comb], 
+  ggplot2[...], 
+  tibble[enframe], 
+  lubridate[...], 
+  ggforce[facet_zoom],
+  oce[...],
+  foreach[...]
+)
+
+source('R/funcs.R')
+
+data(apaobs)
+
+apaobs <- apaobs %>%
+  filter(year(DateTimeStamp) == 2012) %>% 
+  select(-cordat)
+
+# site metadata
+locs <- SWMPr::stat_locs %>% 
+  filter(station_code == 'apacp')
+lat <- locs$latitude
+long <- locs$longitude
+tz <- attr(apaobs$DateTimeStamp, 'tzone')
+
+# setup parallel backend
+ncores <- detectCores()  
+registerDoParallel(cores = ncores - 1)
+
+# get evalcor results to check sun angle/tidal height correlations
+cordatflr <- evalcorflr(apaobs, tz, lat, long, progress = T, plot = F)
+apaobs$cordatflr <- cordatflr
+
+# get regular evalcor results
+cordat <- evalcor(apaobs, tz, lat, long, progress = T, plot = F)
+apaobs$cordat <- cordat
+apaevlcr <- apaobs
+
+save(apaevlcr, file = here('data/apaevlcr.RData'))
+

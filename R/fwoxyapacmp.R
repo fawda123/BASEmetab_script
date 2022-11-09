2a580e704dddad0891b05bdd85cf0efc4eef72b8
fawda123 <mbafs2012@gmail.com>
2022-11-09 17:20:38 -0500

    working on fwoxy apa comparisons for gridded output

--- R/fwoxyapacmp.R
+++ R/fwoxyapacmp.R
@@ -128,4 +128,108 @@ for(i in 1:nrow(grd)){
 
 apagrd <- grd
 save(apagrd, file = 'data/apagrd.RData', compress = 'xz')
-# use left_join of fwdatinp with res to remove interpolated values where there are gaps in the original
+
+# evaluate fit --------------------------------------------------------------------------------
+
+grd <- apagrd %>% 
+  select(-out)
+
+fwdat <- read_csv(here("data/apafwoxy.csv")) 
+
+# fwoxy for comparison
+# convert areal to volumetric, extract b at right time step
+fwdatcmp <- fwdat %>% 
+  mutate(
+    DateTimeStamp = dmy_hms(datet, tz = 'America/Jamaica'),
+    Date = as.Date(DateTimeStamp, tz = 'America/Jamaica'),
+    DO_obs = `oxy,mmol/m3`, 
+    a = `aparam,(mmolO2/m2/d)/(W/m2)` / `ht,m`,
+    Rt_vol = `er,mmol/m2/d` / `ht,m`,
+    Pg_vol = `gpp,mmol/m2/d` / `ht,m`,
+    D = -1 * `gasex,mmol/m2/d` / `ht,m`,
+    b = 100 * 3600 * `kw,m/s` / `wspd2,m2/s2` / (`sc,dimensionless` / 600) ^ -0.5 # (m/s)/(m2/s2) to (cm/hr) / (m2/s2)
+  ) %>% 
+  select(Date, DateTimeStamp, DO_obs, a, b, Pg_vol, Rt_vol, D)
+
+# summary function and ave diff
+sumfun <- function(x){
+  
+  r2 <- lm(EBASE ~ Fwoxy, x) %>% 
+    summary() %>% 
+    .$r.squared
+  r2 <- 100 * r2
+  
+  ts1 <- sum(x$EBASE, na.rm = TRUE)
+  ts2 <- sum(x$Fwoxy, na.rm = TRUE)
+  
+  aved <- 100 * (ts1 - ts2)/((ts1 + ts2)/2)
+  
+  out <- data.frame(r2 = r2, aved = aved)
+  
+  return(out)
+  
+}
+
+
+sumdat <- apagrd %>% 
+  mutate(
+    ind = 1:nrow(.),
+    ests = purrr::pmap(list(ind, out), function(ind, out){
+      
+      cat(ind, '\t')
+      
+      cmp <- inner_join(fwdatcmp, out[[1]], by = c('Date', 'DateTimeStamp')) %>%
+        select(-converge, -dDO, -DO_obs.y, -rsq, -matches('lo$|hi$')) %>%
+        rename(
+          DO_mod.x = DO_obs.x,
+          DO_mod.y = DO_mod
+        ) %>%
+        pivot_longer(!all_of(c('DateTimeStamp', 'Date', 'grp')), names_to = 'var', values_to = 'val') %>%
+        separate(var, c('var', 'mod'), sep = '\\.') %>%
+        mutate(
+          mod = case_when(
+            mod == 'x' ~ 'Fwoxy',
+            mod == 'y' ~ 'EBASE'
+          )
+        ) %>%
+        pivot_wider(names_from = 'mod', values_from = 'val')
+      
+      sumgrp <- cmp %>% 
+        filter(var %in% c('a', 'Rt_vol', 'b')) %>% 
+        group_by(grp, var) %>% 
+        summarise(
+          Fwoxy = mean(Fwoxy, na.rm = T), 
+          EBASE = mean(EBASE, na.rm = T), 
+          .groups = 'drop'
+        ) 
+      sumtms <- cmp %>% 
+        filter(!var %in% c('a', 'Rt_vol', 'b'))
+      
+      sumcmp <- bind_rows(sumgrp, sumtms) %>% 
+        group_by(var) %>% 
+        nest() %>% 
+        summarise(
+          est = purrr::map(data, sumfun)
+        ) %>% 
+        unnest('est')
+      
+      return(sumcmp)
+      
+    })
+  )
+
+toplo <- sumdat %>% 
+  select(-out, -ind, -amean, -rmean, -bmean) %>% 
+  unnest('ests') %>% 
+  mutate(
+    asd = factor(asd),
+    rsd = factor(rsd), 
+    bsd = factor(bsd)
+  ) %>% 
+  filter(!var %in% 'b')
+
+# maybe do as flextable or reactable?
+ggplot(toplo, aes(x = var, y = rsd, fill = r2)) +
+  geom_tile() + 
+  facet_grid(asd ~ bsd)
+

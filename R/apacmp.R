d9ed222052d9c5a2b734a869b58ac040e44a07ee
fawda123 <mbafs2012@gmail.com>
2023-08-04 14:51:10 -0400

    reproc apacmp with correct ebase jags model where all is areal

--- R/apacmp.R
+++ R/apacmp.R
@@ -3,12 +3,16 @@ library(foreach)
 library(doParallel)
 library(here)
 library(WtRegDO)
-library(EBASE)
+# library(EBASE)
+devtools::load_all('../EBASE', helpers = F)
 library(tidyverse)
 
 source(here('R/funcs.R'))
 
 load(file = here('data/apacpdtd.RData'))
+
+# depth at site
+depth <-  apacpdtd$Tide %>% mean()
 
 # Odum, obs ------------------------------------------------------------------------------------
 
@@ -16,11 +20,10 @@ tmp <- apacpdtd %>%
   select(DateTimeStamp, Temp, Sal, DO_mgl = DO_obs, ATemp, BP, WSpd, Tide)
 
 opmetab <- ecometab(tmp, tz = 'America/Jamaica', lat = 29.7021, long = -84.8802, gasex = 'Wanninkhof', gasave = 'daily', metab_units = 'mmol', 
-                    depth_val = 'Tide', instant = T)
+                    depth_val = NULL, depth_vec = depth, instant = T)
 
 # all units to mmol O2 m-2 d-1
 opmetaball <- opmetab %>% 
-  # mutate(Tide = apacpdtd$Tide[-1]) %>% 
   group_by(metab_date) %>% 
   summarize(
     D = mean(D, na.rm = T), 
@@ -43,7 +46,7 @@ ncores <- detectCores()
 cl <- makeCluster(ncores - 2)
 registerDoParallel(cl)
 
-res <- ebase(tmp, interval = 900, H = apacpdtd$Tide, progress = TRUE, n.chains = 4)
+res <- ebase(tmp, interval = 900, H = depth, progress = TRUE, n.chains = 4)
 
 stopCluster(cl)
 
@@ -77,11 +80,10 @@ tmp <- apacpdtd %>%
   select(DateTimeStamp, Temp, Sal, DO_mgl = DO_nrm, ATemp, BP, WSpd, Tide)
 
 opmetab <- ecometab(tmp, tz = 'America/Jamaica', lat = 29.7021, long = -84.8802, gasex = 'Wanninkhof', gasave = 'daily', metab_units = 'mmol', 
-                    depth_val = 'Tide', instant = T)
+                    depth_val = NULL, depth_vec = depth, instant = T)
 
 # all units to mmol O2 m-2 d-1
 opmetaball <- opmetab %>% 
-  # mutate(Tide = apacpdtd$Tide[-1]) %>% 
   group_by(metab_date) %>% 
   summarize(
     D = mean(D, na.rm = T), 
@@ -104,7 +106,7 @@ ncores <- detectCores()
 cl <- makeCluster(ncores - 2)
 registerDoParallel(cl)
 
-res <- ebase(tmp, interval = 900, H = apacpdtd$Tide, progress = TRUE, n.chains = 4)
+res <- ebase(tmp, interval = 900, H = depth, progress = TRUE, n.chains = 4)
 
 stopCluster(cl)
 
